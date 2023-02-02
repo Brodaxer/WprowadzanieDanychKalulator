254de5a9fd4ad5dfeda4bec9af2c88b1f9848fff
Brodaxer <115712630+Brodaxer@users.noreply.github.com>
2023-02-02 13:42:59 +0100

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
 
         Math input = new Math();
-        input.calculate();
         System.out.println(input.calculate());
     }
 }
